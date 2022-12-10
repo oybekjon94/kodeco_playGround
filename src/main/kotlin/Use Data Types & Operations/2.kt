ee5485d898ec41087cc8684512c793e9ff7f6bd0
oybekjon94 <x.oybek94@gmail.com>
2022-12-11 01:07:16 +0900

    [feat::] Add 2 data types

---
+++ src/main/kotlin/Use Data Types & Operations/2.kt
@@ -0,0 +1,20 @@
+package `Use Data Types & Operations`
+fun main(){
+    //val isProgrammingAwesome = true
+    //val isLazinessAwesome = false
+//
+    //println("is programming awesome? $isProgrammingAwesome")
+    //println("is laziness awesome? $isLazinessAwesome")
+
+    val passingGrade = 50
+    val studentGrade = 50
+    val studentPassed = studentGrade >= passingGrade
+    println(studentPassed)
+
+    val studentFailed = studentGrade < passingGrade
+    println(studentFailed)
+
+    val catName = "ozma"
+    val dogName = "cosmos"
+    println(catName == dogName)
+}
