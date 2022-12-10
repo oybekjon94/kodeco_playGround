b6a1d653c1c0632ac04c9a455183fec1d46b3b49
oybekjon94 <x.oybek94@gmail.com>
2022-12-11 00:45:51 +0900

    [feat::] Add 1 data types

--- src/main/kotlin/Use Data Types & Operations/1.kt
+++ src/main/kotlin/Use Data Types & Operations/1.kt
@@ -1,2 +1,28 @@
 package `Use Data Types & Operations`
+/**
+ * we learned main function [main]
+ * */
+fun main(){
+    var age: Int = 32
+    val name: String = "Oybek"
+    val lastname: String = "Kholikov"
+    val weight = 80.1
+    println(name)
+    println("$name is a coder")
+    println(age)
+    age += 1
+    println(age)
+    println("$name $lastname is $age years old and weights $weight kg")
 
+    val fullName = "$lastname, $name"
+    println(fullName)
+
+    val ageAsString = age.toString()
+    println(ageAsString)
+
+    val ageFromString = "35".toInt()
+    println(ageFromString)
+
+    val nameLength = fullName.length
+    println(nameLength)
+}
