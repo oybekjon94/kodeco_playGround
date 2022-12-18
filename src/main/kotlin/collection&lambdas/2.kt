aadec3865c5ec1812f96dd9f37ae4578656fdce1
oybekjon94 <x.oybek94@gmail.com>
2022-12-18 14:51:35 +0900

    [feat::] Add 2 collection&lambdas

--- src/main/kotlin/collection&lambdas/2.kt
+++ src/main/kotlin/collection&lambdas/2.kt
@@ -1,2 +1,13 @@
 package `collection&lambdas`
 
+fun main() {
+    val birthday = Triple(2,3,1991)
+    val person = Pair("Damini",birthday)
+    val nameUsingOrdering = person.first
+    val birthdayUsingOrdering = person.second
+    val (name,bday) = person
+    val (dayOfBirth, birthMonth, _) = bday
+    val yearOfBirth = birthdayUsingOrdering.third
+
+    println("$name was born on $dayOfBirth/$birthMonth, year unknown!")
+}
