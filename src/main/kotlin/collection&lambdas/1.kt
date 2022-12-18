b01a1dd58e3f14b1870f21254bf90d30bd710f29
oybekjon94 <x.oybek94@gmail.com>
2022-12-18 14:30:26 +0900

    [feat::] Add 1 collection&lambdas

---
+++ src/main/kotlin/collection&lambdas/1.kt
@@ -0,0 +1,26 @@
+package `collection&lambdas`
+
+/*
+Collections :
+-A single constructor which can hold multiple values of the same type
+-can work with any type
+-can be iterated over
+ */
+/*
+type of collections:
+-array
+-list
+-set
+-map
+ */
+/*
+Generics<?>
+-classes & functions operating on yet undefined types
+-types are determined when creating objects
+ */
+/*
+pairs & triples
+-group of two to three pieces of data
+-can can represent structured data
+ */
+
--- src/main/kotlin/functions&nullability/5.kt
+++ src/main/kotlin/functions&nullability/5.kt
@@ -1,5 +1,37 @@
 package `functions&nullability`
 
-fun main(){
+/*
+Challenge 1:
+-create a function which takes in two
+parameters - a name and a last name
+-because not everyone has a last name,
+leave the lastName parameter to be an empty
+String if it is not passed in
+-Then return how long the person's
+full name is
 
+Challenge 2:
+Overload the function from the first
+challenge by adding a list of Strings
+parameters , for middle names
+in case someone has one or more middle name
+
+use the function to return the full name
+length , for a name with and without
+middle names.
+remember to use named arguments of needed
+ */
+fun main(){
+   fun getFullNameLength(name: String, lastName: String = "")=
+       name.length + lastName.length
+
+    fun getFullNameLength(name: String,middleName: String = "", lastName: String = ""): Int{
+        return name.length + middleName.length + lastName.length
+    }
+
+    val nameLength = getFullNameLength("Ayo","Balogun")
+    println(nameLength)
+
+    val length = getFullNameLength("Damini","EbunoLuwa","Oquli")
+    println(length)
 }
