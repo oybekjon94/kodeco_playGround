6ab7590e079bf3eb8b4fcc2236626db37bc8a02c
oybekjon94 <x.oybek94@gmail.com>
2022-12-28 21:41:03 +0900

    [feat::] Add 4 collection&lambdas

--- src/main/kotlin/collection&lambdas/4.kt
+++ src/main/kotlin/collection&lambdas/4.kt
@@ -9,4 +9,26 @@ fun main(){
     println(countries.size)
     println(countries[3])
     println(countries.get(3))
+
+    val sizedArray = Array(10) {""}
+    val emptyArray = emptyArray<String>()
+
+    val arrayOfInt = intArrayOf(2,3,4,5,6,7)
+    println(arrayOfInt)
+
+    val intArray = IntArray(10)
+    println(intArray)
+
+    println(countries.lastIndex)
+    println(countries.last())
+    println(countries.first())
+
+    val currentCountry = "Mauritius"
+    println(currentCountry.contains(currentCountry))
+
+    val isWestAfricanCountry =  currentCountry in countries
+    val isNotWestAfricanCountry =  currentCountry !in countries
+
+    println(isWestAfricanCountry)
+    println(isNotWestAfricanCountry)
 }
