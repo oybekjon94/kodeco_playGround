aeb61170d97e71791b4dc26c8275d76d2ff50e72
oybekjon94 <x.oybek94@gmail.com>
2022-12-31 14:33:42 +0900

    [feat::] Add 1 collection

--- src/main/kotlin/exploreMoreCollections/1.kt
+++ src/main/kotlin/exploreMoreCollections/1.kt
@@ -19,4 +19,35 @@ fun main(){
         "content/type" to "application/json"
     )
     println(authenticationHeaders)
+
+    //----Maps
+
+    moviesColection["Horror"] = mutableListOf()
+    println(moviesColection)
+
+    moviesColection["Horror"] = mutableListOf("Get out")
+    println(moviesColection)
+
+    moviesColection["Horror"]?.add("Middsommar")
+    println(moviesColection)
+
+    val removeActionMovies = moviesColection.remove("Action")
+    println(removeActionMovies)
+    println(moviesColection)
+
+//    authenticationHeaders["Authorization"]  = "new-api-key"
+    moviesColection["Horror"]?.add("It Comes At Night")
+    moviesColection["Comedy"] = mutableListOf("ted")
+
+    for (key in moviesColection.keys){
+        println(key)
+    }
+
+    for (value in moviesColection.values){
+        println(value)
+    }
+
+    for ((key, value) in moviesColection){
+        println("Movies in the $key genre you own are: $value")
+    }
 }
---
+++ src/main/kotlin/exploreMoreCollections/2.kt
@@ -0,0 +1,5 @@
+package exploreMoreCollections
+
+fun main(){
+
+}
